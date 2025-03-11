b9be039d30f4d8cb8f1d960d6b36e002921e817b
Dalysha <Dalyshajonis@outlook.com>
2025-03-11 13:56:13 +0100

    start van de dag

--- contactpagina.php
+++ contactpagina.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="en">
 
 <head>
   <meta charset="utf-8">
@@ -35,10 +35,15 @@
   <div class="sidebar-1">
     <div class="sidebar_left">
       <a href="" id="sidebar_link" href="url">link text</a><br>
-      <a id="sidebar_link" href="url">link text</a><br>
-      <a id="sidebar_link" href="url">link text</a><br><br>
-      <p>Free Products</p>
-      <p>Under 30</p>
+      <a href="" id="sidebar_link" href="url">link text</a><br>
+      <a href="" id="sidebar_link" href="url">link text</a><br><br>
+      <h3>Categories</h3>
+      <button>Free products</button>
+        <button>Products under $30</button>
+        <div class="slidecontainer">
+          <p>Custom range slider:</p>
+          <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+        </div>
     </div>
   </div>
   <div class="sidebar-2">sidebar-2</div>
@@ -46,10 +51,10 @@
   <div class="main">
     <div class="contact_area">
     <div id="titel_contact">
-      <h1>Maak hier je afspraak.</h1>
-      <p>Vul hier je gegevens in.</p>
+      <h1>Stuur me hier een berichtje!</h1>
+      <p>Vul hier je gegevens in.</p><br>
     </div>
-      <form action="doelbestand.php" method="POST" class="form">
+      <form class="" ction="/" method="POST" class="form">
         <label class="omschrijving" for="voornaam">Voornaam:</label>
         <div id="input"><input type="text" tabindex="1" name="voornaam" placeholder="Voornaam" value="" required></div><br>
 
@@ -70,43 +75,7 @@
         <label class="omschrijving" for="geboorte_datum">Geboortedatum:</label>
         <div id="input"><input type="date" tabindex="5" name="geboorte_datum" placeholder="Geboortedatum"></div><br>
 
-        <label class="omschrijving" for="datum">Datum:</label>
-        <div id="input"><input type="date" tabindex="6" name="datum" placeholder="Datum"></div><br>
-
-        <label class="omschrijving" for="tijd">Tijd:</label>
-        <div id="input"><input type="time" tabindex="7" name="tijd" placeholder="Tijd"></div><br>
-
-        <label class="omschrijving" for="personen">Kom je alleen?:</label>
-        <div id="input"><input type="radio" tabindex="8" id="Ja" name="personen" value="Ja">
-          <label for="Ja">Ja, ik kom alleen.</label>
-          <input type="radio" tabindex="9" id="Nee" name="personen" value="Nee">
-          <label for="Nee">Nee, ik kom met iemand.</label>
-        </div><br>
-
-        <label class="omschrijving" for="behandeling">Type behandeling:</label>
-        <div id="input" input type="radio" tabindex="10"  id="behandeling" name="behandeling" value="behandeling"
-          placeholder="behandeling">
-          <label for="acryl">Acryl.</label>
-          <input type="radio" tabindex="11" id="gel" name="behandeling" value="gel">
-          <label for="Gel">Gel.</label> 
-          <input type="radio" id="Biab" tabindex="12" name="behandeling" value="Biab">
-          <label for="biab">Biab.</label>
-          <input type="radio" tabindex="13" id="Manicure" name="behandeling" value="Manicure">
-          <label for="Manicure">Manicure.</label>
-          <input type="radio" tabindex="14" id="Pedicure" name="behandeling" value="Pedicure">
-          <label for="Pedicure">Pedicure.</label>
-          <input type="radio" tabindex="15" id="P+M" name="behandeling" value="P+M">
-          <label for="Pedicure + Manicure">Pedicure + Manicure.</label>
-        </div><br>
-
-        <label class="omschrijving" for="palet" placeholder="Palet">kleurenpalet:</label>
-        <div id="input"><input type="color" tabindex="16"  id="kleurenpalet" name="palet" value="" width="70px" />
-          <input type="color" tabindex="17" id="kleurenpalet" name="palet" value="" width="70px" />
-          <input type="color" tabindex="18" id="kleurenpalet" name="palet" value="" width="70px" />
-          <input type="color" tabindex="19" id="kleurenpalet" name="palet" value="" width="70px" />
-        </div><br>
-
-        <label class="omschrijving groot" for="Rekening">waar moet ik rekening mee houden?:</label>
+        <label class="omschrijving groot" for="Rekening">Opmerkingen?</label>
         <div id="input"><textarea name="omschrijving" tabindex="20" >Hou rekening met...</textarea></div><br>
 
         <label class="omschrijving" for="nieuwsbrief">Nieuwsbrief:</label>
@@ -114,6 +83,7 @@
           mij aanmelden voor de nieuwsbrief met $10 korting op mijn volgende bestelling.</label></div><br>
         <input type="Submit" tabindex="22" class="neonbut">
       </form>
+      </div>
       </div>
       </div>
   
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="en">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title></title>
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <meta name="description" content="">
@@ -26,7 +25,7 @@
 <body>
   <div class="container">
     <div class="header">
-      <img id="logo" href="index.php" src="assets/img/logoWebshop.JPEG" alt="/">
+      <img id="logo" src="assets/img/logoWebshop.JPEG" alt="/">
     </div>
     <div class="header_2">
       <a href="index.php" class="header2_tekst">Products</a>
@@ -34,9 +33,8 @@
     </div>
     <div class="sidebar-1">
       <div class="sidebar_left">
-        <a href="https://www.youtube.com/" id="sidebar_link" href="url">link text</a><br>
-        <a href="https://www.instagram.com/" id="sidebar_link" href="url">link text</a><br><br>
-        <!-- <a href="" id="sidebar_link" href="url">link text</a><br><br> -->
+        <a href="https://www.youtube.com/" class="sidebar_link">link text</a><br>
+        <a href="https://www.instagram.com/" class="sidebar_link">link text</a><br><br>
         <h3>Categories</h3>
         <button>Free products</button>
         <button>Products under $30</button>
@@ -49,145 +47,146 @@
     <div class="sidebar-2">sidebar-2</div>
     <div class="main">
       <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
         <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
+        <p class="price">$0</p>
         <p>The basics of digital marketing.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/3 Months Of Social Media Content Cover kopie.png" alt="/">
+        <img class="card_img" src="assets/img/3_Months_Of_Social_Media_Content_Cover_kopie.png" alt="/">
         <h1>3 Months Of Social Media Content</h1>
-        <p class="price"></p>
+        <p class="price">$0</p>
         <p>3 Months of social media cons3tent planned out for you</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/130 Faceless Video's kopie.png" alt="/">
+        <img class="card_img" src="assets/img/130_Faceless_Video's_kopie.png" alt="/">
         <h1>130 Faceless Video's</h1>
-        <p class="price"></p>
+        <p class="price">$25</p>
         <p>130 Faceless Video's ready to use for your faceless marketing.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/300+ Canva Templates kopie.png" alt="/">
+        <img class="card_img" src="assets/img/300+_Canva_Templates_kopie.png" alt="/">
         <h1>300+ Canva Templates</h1>
-        <p class="price"></p>
+        <p class="price">$30</p>
         <p>300+ Canva Templates. Easy to use to create an ebook</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/The No Face Money Guide Cover.png" alt="/">
+        <img class="card_img" src="assets/img/The_No_Face_Money_Guide_Cover.png" alt="/">
         <h1>The No Face Money Guide</h1>
-        <p class="price"></p>
+        <p class="price">$37</p>
         <p>Ultimate guide to make money without showing your face.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/How To Launch Cover kopie.png" alt="/">
+        <img class="card_img" src="assets/img/How_To_Launch_Cover_kopie.png" alt="/">
         <h1>The DFY Boss Up Business Launch Guide</h1>
-        <p class="price"></p>
+        <p class="price">$27</p>
         <p>The best wy to launch your business.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/Swipe Up To Profit Cover kopie.png" alt="/">
+        <img class="card_img" src="assets/img/Swipe_Up_To_Profit_Cover_kopie.png" alt="/">
         <h1>Swipe Up To Profit</h1>
-        <p class="price"></p>
+        <p class="price">$20</p>
         <p>Get sales by making the right reels.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/The Complete Content Handbook Cover kopie.png" alt="/">
+        <img class="card_img" src="assets/img/The_Complete_Content_Handbook_Cover_kopie.png" alt="/">
         <h1>The Complete Content Handbook</h1>
-        <p class="price"></p>
+        <p class="price">$20</p>
         <p>Don't Know what to post? Nothing is working? Get the The Complete Content Handbook.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
         <img class="card_img"
-          src="assets/img/The DFY Digital Marketing Business Blueprint Create, Launch And Profit. kopie.png" alt="/">
+          src="assets/img/The_DFY_Digital_Marketing_Business_Blueprint_Create,_Launch_And_Profit.kopie.png" alt="/">
         <h1>The DFY Digital Marketing Business Blueprint</h1>
-        <p class="price"></p>
+        <p class="price">$47</p>
         <p>Create, Launch And Profit youre business guide.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/The DFY Vendor Vault List Of Vendors kopie.png" alt="/">
+        <img class="card_img" src="assets/img/The_DFY_Vendor_Vault_List_Of_Vendors_kopie.png" alt="/">
         <h1>The DFY Vendor Vault</h1>
-        <p class="price"></p>
+        <p class="price">$18</p>
         <p>A list of vendors to start your business.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/The Ultimate Reels Strategy kopie.png" alt="/">
+        <img class="card_img" src="assets/img/The_Ultimate_Reels_Strategy_kopie.png" alt="/">
         <h1>The Ultimate Reels Strategy</h1>
-        <p class="price"></p>
+        <p class="price">$25</p>
         <p>Master the ultimate reels strategy to get more sales.</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
+        <h1>DFY Digital Product Freebie Guide</h1>
+        <p class="price">$0</p>
+        <p>Some text about the jeans..</p>
+        <p><button>Add to cart</button></p>
+      </div>
+      <div class="card">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
+        <h1>DFY Digital Product Freebie Guide</h1>
+        <p class="price">$0</p>
+        <p>Some text about the jeans..</p>
+        <p><button>Add to cart</button></p>
+      </div>
+      <div class="card">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
+        <h1>DFY Digital Product Freebie Guide</h1>
+        <p class="price">$0</p>
+        <p>Some text about the jeans..</p>
+        <p><button>Add to cart</button></p>
+      </div>
+      <div class="card">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
+        <h1>DFY Digital Product Freebie Guide</h1>
+        <<p class="price">$0</p>
+        <p>Some text about the jeans..</p>
+        <p><button>Add to cart</button></p>
+      </div>
+      <div class="card">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
+        <h1>DFY Digital Product Freebie Guide</h1>
+        <p class="price">$0</p>
+        <p>Some text about the jeans..</p>
+        <p><button>Add to cart</button></p>
+      </div>
+      <div class="card">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
         <h1>DFY Digital Product Freebie Guide</h1>
         <p class="price"></p>
         <p>Some text about the jeans..</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
         <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
+        <p class="price">$0</p>
         <p>Some text about the jeans..</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
         <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
+        <p class="price">$0</p>
         <p>Some text about the jeans..</p>
         <p><button>Add to cart</button></p>
       </div>
       <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
+        <img class="card_img" src="assets/img/Digital_Marketing Freebie_Guide_kopie.png" alt="/">
         <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
+        <p class="price">$0</p>
         <p>Some text about the jeans..</p>
         <p><button>Add to cart</button></p>
       </div>
-      <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
-        <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
-        <p>Some text about the jeans..</p>
-        <p><button>Add to cart</button></p>
-      </div>
-      <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
-        <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
-        <p>Some text about the jeans..</p>
-        <p><button>Add to cart</button></p>
-      </div>
-      <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
-        <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
-        <p>Some text about the jeans..</p>
-        <p><button>Add to cart</button></p>
-      </div>
-      <div class="card">
-        <img class="card_img" src="assets/img/Digital Marketing Freebie Guide kopie.png" alt="/">
-        <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
-        <p>Some text about the jeans..</p>
-        <p><button>Add to cart</button></p>
-      </div>
-      <div class="card">
-        <img class="card_img" src="assets/img/300 PLR Products  kopie.png" alt="/">
-        <h1>DFY Digital Product Freebie Guide</h1>
-        <p class="price"></p>
-        <p>Some text about the jeans..</p>
-        <p><button>Add to cart</button></p>
       </div>
     </div>
     <div class="footer">
@@ -196,10 +195,5 @@
         </p>
       </div>
     </div>
-
-
-
-    <script src="assets/js/app.js"></script>
 </body>
-
 </html>
