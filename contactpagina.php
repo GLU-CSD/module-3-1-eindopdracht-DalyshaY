61e3859f2373df4d2f54505e5ce4c52344840222
Dalysha <Dalyshajonis@outlook.com>
2025-03-14 10:19:31 +0100

    aanpassingen

--- contactpagina.php
+++ contactpagina.php
@@ -54,37 +54,38 @@
         </div>
         <form action="/" method="POST" class="form">
           <label class="omschrijving" for="voornaam">Voornaam:</label>
-          <div class="input"><input type="text" tabindex="1" name="voornaam" placeholder="Voornaam" required></div><br>
+          <div class="input"><input type="text" id="voornaam" name="voornaam" placeholder="Voornaam" required></div><br>
 
           <label class="omschrijving" for="tv">Tussenvoegsel (optioneel):</label>
-          <div class="input"><input type="text" tabindex="2" name="tv" placeholder="Tussenv." size="20"></div><br>
+          <div class="input"><input type="text" id="tv" name="tv" placeholder="Tussenv." size="20"></div><br>
 
           <label class="omschrijving" for="achternaam">Achternaam:</label>
-          <div class="input"><input type="text" tabindex="3" name="achternaam" placeholder="Achternaam" required></div>
-          <br>
+          <div class="input"><input type="text" id="achternaam" name="achternaam" placeholder="Achternaam" required>
+          </div><br>
 
           <label class="omschrijving" for="email">E-mailadres:</label>
-          <div class="input"><input type="email" tabindex="4" id="email" name="email" placeholder="random@gmail.com"
+          <div class="input"><input type="email" id="email" name="email" placeholder="random@gmail.com"
               pattern="[a-z0-9.%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></div><br>
 
           <label class="omschrijving" for="telnummer">Telefoonnummer:</label>
-          <div class="input"><input type="tel" tabindex="5" id="telnummer" name="telnummer" pattern="[0-9]{5,20}"
+          <div class="input"><input type="tel" id="telnummer" name="telnummer" pattern="[0-9]{5,20}"
               placeholder="Telefoonnummer"></div><br>
 
           <label class="omschrijving" for="geboorte_datum">Geboortedatum:</label>
-          <div class="input"><input type="date" tabindex="6" name="geboorte_datum"></div><br>
+          <div class="input"><input type="date" id="geboorte_datum" name="geboorte_datum"></div><br>
 
           <label class="omschrijving groot" for="omschrijving">Opmerkingen?</label>
-          <div class="input"><textarea name="omschrijving" tabindex="7">Hou rekening met...</textarea></div><br>
+          <div class="input"><textarea id="omschrijving" name="omschrijving">Hou rekening met...</textarea></div><br>
 
           <div class="input">
-            <input type="checkbox" tabindex="8" id="nieuwsbrief" name="nieuwsbrief">
+            <input type="checkbox" id="nieuwsbrief" name="nieuwsbrief">
             <label for="nieuwsbrief">Ja, ik wil mij aanmelden voor de nieuwsbrief met $10 korting op mijn volgende
               bestelling.</label>
           </div><br>
 
-          <input type="submit" tabindex="9" class="neonbut" value="Verzenden">
+          <input type="submit" class="neonbut" value="Verzenden">
         </form>
+
       </div>
     </div>
   </div>
